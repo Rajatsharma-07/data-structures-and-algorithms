486a4a9a0cb469c3222c3241dfd2d2243831ae98
Rajatsharma-07 <rajatsharmabkn07@gmail.com>
2026-03-23 01:18:33 +0530

    rotate image problem solved

---
+++ rotate_image.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        void rotate(vector<vector<int>>& matrix) {
+            for(int i = 0; i < matrix.size(); i++){
+                for (int j = 0; j < matrix.size(); j++) {
+                    if (i != j && i < j) {
+                        swap(matrix[i][j], matrix[j][i]);
+                    }
+                }
+            }
+    
+            for (int i = 0; i < matrix.size(); i++) {
+                for (int j = 0; j < matrix.size(); j++) {
+                    if (j < matrix.size() / 2) {
+                        swap(matrix[i][j], matrix[i][matrix.size() - 1 - j]);
+                    }
+                }
+            }
+        }
+    };
