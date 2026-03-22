6a6d238a9b52bcb3cee5b7632b0b61f43e1023b0
Rajatsharma-07 <rajatsharmabkn07@gmail.com>
2026-03-23 02:13:21 +0530

    find duplicate problem solved

---
+++ find_duplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public:
+        int findDuplicate(vector<int>& nums) {
+            int fast = 0, slow = 0;
+    
+            while (true) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+                if (fast == slow) {
+                    break;
+                }
+            }
+    
+            slow = 0;
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+    
+            return fast;
+        }
+    };
