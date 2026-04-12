daa38951917044b3d0f6fa83db62bad8f18c6885
Rajatsharma-07 <rajatsharmabkn07@gmail.com>
2026-04-12 16:25:09 +0530

    Add two numbers as a linked list problem solved

---
+++ add_two_numbers_as_linked_list.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans = new ListNode(0);
+        ListNode* dummy = ans;
+        int carry = 0;
+        
+        while(l1 != NULL || l2 != NULL || carry) {
+            int sum = 0;
+            if (l1) {
+                sum += l1 -> val;
+                l1 = l1 -> next;
+            }
+
+            if (l2) {
+                sum += l2 -> val;
+                l2 = l2 -> next;
+            }
+
+            if (carry) {
+                sum += carry;
+            }
+
+            carry = sum / 10;
+            ListNode* dummy2 = new ListNode(sum % 10);
+            dummy -> next = dummy2;
+            dummy = dummy -> next;
+        }
+
+        return ans -> next;
+    }
+};
