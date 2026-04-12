28586d9e26e1ed8a625c66d5f4aef2fe9c5fd53a
Rajatsharma-07 <rajatsharmabkn07@gmail.com>
2026-04-12 21:00:19 +0530

    Check if a likend list is a palindrom problem solved

---
+++ check_palindrom_in_linked_list.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* reverseLinkedList(ListNode* node) {
+        ListNode* prev = NULL;
+        ListNode* nex = NULL;
+        while(node) {
+            nex = node -> next;
+            node -> next = prev;
+            prev = node;
+            node = nex;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head -> next == NULL){
+            return true;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast -> next != NULL && fast -> next -> next != NULL) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+
+        slow -> next = reverseLinkedList(slow -> next);
+        slow = slow -> next;
+
+        while (slow) {
+            if (slow -> val != head -> val) {
+                return false;
+            }
+            slow = slow -> next;
+            head = head -> next;
+        }
+        return true;
+    }
+};
