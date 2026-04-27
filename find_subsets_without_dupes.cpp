980fe5d2f34eb7107ecc9580534998455b41f4cf
Rajatsharma-07 <rajatsharmabkn07@gmail.com>
2026-04-28 01:17:51 +0530

    find subsets without duplicates problem solved

---
+++ find_subsets_without_dupes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        void findSubsets(int index, vector<int>& ds, vector<vector<int>>& ans, vector<int>& nums) {
+            ans.push_back(ds);
+            for(int i = index; i < nums.size(); i++ ) {
+                if (i != index && nums[i] == nums[i - 1]) continue;
+                ds.push_back(nums[i]);
+                findSubsets(i + 1, ds, ans, nums);
+                ds.pop_back();
+            }
+        }
+        vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+            vector<vector<int>> ans;
+            vector<int> ds;
+            sort(nums.begin(), nums.end());
+            findSubsets(0, ds, ans, nums);
+            return ans;
+        }
+    };
